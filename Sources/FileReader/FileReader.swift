4408e4ccb60e78143c977c98c0e0180e5b62052c
HYUNSIC_KIM <hyunsic@HYUNSIC-KIM-MacBook-Pro.local>
2018-04-22 16:59:47 +0900

    Initial Commit

--- Sources/FileReader/FileReader.swift
+++ Sources/FileReader/FileReader.swift
@@ -1,6 +1,8 @@
 import Foundation
+
+
 class FileReader {
-	public static func read(fileName: String)->String?{
+	public static func read(fileName: String)->String? {
 	let fileManager = FileManager.default
 	let currentDirectoryURL = URL(fileURLWithPath:fileManager.currentDirectoryPath)
 	let fileURL = currentDirectoryURL.appendingPathComponent(fileName)
